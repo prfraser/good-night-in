0676b9a70a2485279544b15e38712415d09704df
calebharnell <calebharnell@hotmail.com>
2017-09-24 13:25:43 +1000

    All functions complete

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,20 +1,47 @@
 class CocktailsController < ApplicationController
   def new
+    @cocktail = Cocktail.new
   end
+
   def index
     @cocktails = Cocktail.all
   end
+
   def create
     @cocktail = Cocktail.new(cocktail_params)
+
    if @cocktail.save
      redirect_to @cocktail
    else
      render 'new'
    end
   end
+
   def show
     @cocktail = Cocktail.find(params[:id])
   end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+
+    if @cocktail.update(cocktail_params)
+      redirect_to @cocktail
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+
+    redirect_to cocktails_path
+  end
+
   private
     def cocktail_params
       params.require(:cocktail).permit(:title, :ingredients, :method)

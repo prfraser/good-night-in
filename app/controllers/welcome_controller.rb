224739e96d6798df9cfe29b856ee825ae2dc0992
philip fraser <pr.fraser3@gmail.com>
2017-10-14 20:10:10 +1000

    messing with the search controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
 	  @cocktails = Cocktail.search(params[:term])
+	  Cocktail.joins(:cocktail_ingredients).where(cocktail_ingredients: { ingredient_id: 1, cocktail_id: CocktailIngredient.where(ingredient_id: 4).select(:cocktail_id) })
 	end
 
   private
